eea1616dac5d0b62a8eff94263d537b4da0c2d45
Guillaume Falvet <guillaume.falvet@outlook.com>
2024-04-18 20:41:58 +0200

    🧪 test : env spec

---
+++ src/shared/models/env.spec.ts
@@ -0,0 +1,19 @@
+import { Env } from './env';
+
+describe('Env', () => {
+  it('should have the correct values', () => {
+    expect(Env.appStage).toBe('STAGE');
+    expect(Env.appPort).toBe('PORT');
+    expect(Env.appHost).toBe('HOST');
+    expect(Env.dataBaseType).toBe('postgres');
+    expect(Env.dataBaseHost).toBe('DB_HOST');
+    expect(Env.dataBasePort).toBe('DB_PORT');
+    expect(Env.dataBaseUsername).toBe('DB_USERNAME');
+    expect(Env.dataBasePassword).toBe('DB_PASSWORD');
+    expect(Env.database).toBe('DB_DATABASE');
+    expect(Env.jwtAccessTokenSecret).toBe('JWT_ACCESS_SECRET');
+    expect(Env.jwtRefreshTokenSecret).toBe('JWT_REFRESH_SECRET');
+    expect(Env.jwtAccessTokenExpiration).toBe('JWT_ACCESS_TOKEN_EXPIRATION');
+    expect(Env.jwtRefreshTokenExpiration).toBe('JWT_REFRESH_TOKEN_EXPIRATION');
+  });
+});
