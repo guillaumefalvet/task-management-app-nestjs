b524d26889e452d94c69bcb5ca5787b975e89b1f
Guillaume Falvet <guillaume.falvet@outlook.com>
2024-04-20 04:10:01 +0200

    ✨ feat : defined each column type and specificity

--- src/modules/auth/entities/user.entity.ts
+++ src/modules/auth/entities/user.entity.ts
@@ -1,18 +1,20 @@
-import { Task } from '../../tasks/entities/task.entity';
 import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+
+// - Entities - //
+import { Task } from 'src/modules/tasks/entities/task.entity';
 
 @Entity()
 export class User {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
-  @Column({ unique: true })
+  @Column({ unique: true, type: 'varchar', length: 30 })
   username: string;
 
-  @Column()
+  @Column({ type: 'varchar', nullable: false })
   password: string;
 
-  @Column()
+  @Column({ type: 'varchar', length: 30, nullable: true })
   refreshToken: string;
 
   @OneToMany((_type) => Task, (task) => task.user, { eager: true })
--- src/modules/tasks/entities/task.entity.ts
+++ src/modules/tasks/entities/task.entity.ts
@@ -1,20 +1,24 @@
 import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
-import { TaskStatus } from '../../../shared/models/task-status';
-import { User } from '../../auth/entities/user.entity';
 import { Exclude } from 'class-transformer';
+
+// - Entities - //
+import { User } from 'src/modules/auth/entities/user.entity';
+
+// - Models - //
+import { TaskStatus } from 'src/shared/models/task-status';
 
 @Entity()
 export class Task {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
-  @Column()
+  @Column({ type: 'varchar', length: 30 })
   title: string;
 
-  @Column()
+  @Column({ type: 'varchar', length: 30 })
   description: string;
 
-  @Column()
+  @Column({ type: 'varchar', length: 30 })
   status: TaskStatus;
 
   @ManyToOne((_type) => User, (user) => user.tasks, { eager: false })
