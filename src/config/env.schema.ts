bc5b61b8948726cabf45d7f1ff1a6e0700356df2
Guillaume Falvet <guillaume.falvet@outlook.com>
2024-04-20 16:32:56 +0200

    📚 docs : seperated env validation with comments

--- src/config/env.schema.ts
+++ src/config/env.schema.ts
@@ -2,38 +2,36 @@ import * as Joi from '@hapi/joi';
 
 // - Models - //
 import { EnvEnum } from 'src/shared/models/env';
+import { DatabaseTypeEnum } from 'src/shared/models/database';
 
 // - Constants - //
 import {
   HTTPS_PROTOCOL,
   HTTP_PROTOCOL,
 } from 'src/shared/constants/constant-network';
-import { DatabaseTypeEnum } from 'src/shared/models/database';
 
 /**
  * Represents the validation schema for the application env.
  */
 export const envFileValidationSchema = Joi.object({
+  // - Application - //
   [EnvEnum.appPort]: Joi.number().default(3000).required(),
   [EnvEnum.appStage]: Joi.string().required(),
   [EnvEnum.appHost]: Joi.string().required(),
   [EnvEnum.appHttpProtocol]: Joi.string()
     .valid(HTTPS_PROTOCOL, HTTP_PROTOCOL)
     .required(),
-  [EnvEnum.dataBaseType]: Joi.string()
-    .required()
-    .valid(...Object.values(DatabaseTypeEnum)),
-  [EnvEnum.dataBaseHost]: Joi.string().required(),
-  [EnvEnum.dataBasePort]: Joi.number().default(5432).required(),
-  [EnvEnum.dataBaseUsername]: Joi.string()
-    .default(EnvEnum.dataBaseType)
-    .required(),
-  [EnvEnum.dataBasePassword]: Joi.string()
-    .default(EnvEnum.dataBaseType)
-    .required(),
-  [EnvEnum.database]: Joi.string().required(),
   [EnvEnum.jwtAccessTokenSecret]: Joi.string().required().min(8).max(64),
   [EnvEnum.jwtRefreshTokenSecret]: Joi.string().required().min(8).max(64),
   [EnvEnum.jwtAccessTokenExpiration]: Joi.string().required(),
   [EnvEnum.jwtRefreshTokenExpiration]: Joi.string().required(),
+  // - Database - //
+  [EnvEnum.dataBaseType]: Joi.string()
+    .valid(...Object.values(DatabaseTypeEnum))
+    .required(),
+  [EnvEnum.dataBaseHost]: Joi.string().required(),
+  [EnvEnum.dataBasePort]: Joi.number().default(5432).required(),
+  [EnvEnum.dataBaseUsername]: Joi.string().required(),
+  [EnvEnum.dataBasePassword]: Joi.string().required(),
+  [EnvEnum.database]: Joi.string().required(),
 });
