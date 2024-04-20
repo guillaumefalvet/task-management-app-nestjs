b6a5025bc290a13d6635a051df107dcb1a6d83f7
Guillaume Falvet <guillaume.falvet@outlook.com>
2024-04-20 15:17:02 +0200

    ♻️ refactor : http env

--- src/config/env.schema.ts
+++ src/config/env.schema.ts
@@ -3,6 +3,12 @@ import * as Joi from '@hapi/joi';
 // - Models - //
 import { Env } from 'src/shared/models/env';
 
+// - Constants - //
+import {
+  HTTPS_PROTOCOL,
+  HTTP_PROTOCOL,
+} from 'src/shared/constants/constant-network';
+
 /**
  * Represents the validation schema for the application env.
  */
@@ -10,6 +16,9 @@ export const envFileValidationSchema = Joi.object({
   [Env.appPort]: Joi.number().default(3000).required(),
   [Env.appStage]: Joi.string().required(),
   [Env.appHost]: Joi.string().required(),
+  [Env.appHttpProtocol]: Joi.string()
+    .valid(HTTPS_PROTOCOL, HTTP_PROTOCOL)
+    .required(),
   [Env.dataBaseHost]: Joi.string().required(),
   [Env.dataBasePort]: Joi.number().default(5432).required(),
   [Env.dataBaseUsername]: Joi.string().default(Env.dataBaseType).required(),
