a280f73b2613b95bc6f02a148c6c80deb2ad45f6
Guillaume Falvet <guillaume.falvet@outlook.com>
2024-04-20 04:15:09 +0200

    🔧 config : modifed the structure of the typeorm config

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,8 +1,24 @@
+import { ConfigService } from '@nestjs/config';
 import { TypeOrmModuleAsyncOptions } from '@nestjs/typeorm';
 import { config } from 'dotenv';
-import { DataSource } from 'typeorm';
-import { options } from './data-source';
-config(); // Load the environment variables from the .env file
+import { DataSource, DataSourceOptions } from 'typeorm';
+
+// - Models - //
+import { Env } from 'src/shared/models/env';
+
+config();
+const configService = new ConfigService();
+export const options: DataSourceOptions = {
+  type: Env.dataBaseType,
+  host: configService.getOrThrow<string>(Env.dataBaseHost),
+  port: configService.getOrThrow<number>(Env.dataBasePort),
+  database: configService.getOrThrow<string>(Env.database),
+  username: configService.getOrThrow<string>(Env.dataBaseUsername),
+  password: configService.getOrThrow<string>(Env.dataBasePassword),
+  entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+  migrations: [__dirname + '/../database/migrations/*{.ts,.js}'],
+};
+
 export const typeOrmAsyncConfig: TypeOrmModuleAsyncOptions = {
   useFactory: async () => {
     return {
@@ -16,8 +32,7 @@ export const typeOrmAsyncConfig: TypeOrmModuleAsyncOptions = {
     };
   },
 };
-// this is used for migrations
-// God forgive me for this
 export default new DataSource({
   ...options,
+  logging: true,
 });
