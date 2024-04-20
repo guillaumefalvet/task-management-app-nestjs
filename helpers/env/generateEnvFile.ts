1ab2e63fb216d8372981e330dab199c0ceef58b8
Guillaume Falvet <guillaume.falvet@outlook.com>
2024-04-20 04:13:43 +0200

    ✨ feat : function to generate a new Env file

---
+++ helpers/env/generateEnvFile.ts
@@ -0,0 +1,32 @@
+import * as fs from 'fs';
+import * as readlineSync from 'readline-sync';
+
+// - Models - //
+import { Env } from 'src/shared/models/env';
+
+function generateEnvFile() {
+  if (fs.existsSync('.env')) {
+    console.log('.env file already exists. Skipping generation.');
+    return;
+  }
+
+  let envContent = '';
+
+  for (const key in Env) {
+    if (
+      Object.prototype.hasOwnProperty.call(Env, key) &&
+      key !== 'dataBaseType' &&
+      key !== 'appStage'
+    ) {
+      const enumValue = Env[key as keyof typeof Env];
+      const userInput = readlineSync.question(`Enter value for ${enumValue}: `);
+      envContent += `${enumValue}=${userInput}\n`;
+    }
+  }
+
+  fs.writeFileSync('.env', envContent);
+
+  console.log('.env file generated successfully.');
+}
+
+generateEnvFile();
