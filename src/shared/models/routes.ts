7a52b71fe8bd6d69d6ee280cfad03bbc5092f1c1
Guillaume Falvet <guillaume.falvet@outlook.com>
2024-04-20 17:53:35 +0200

    🐞 fix : missing : for the param

--- src/shared/models/routes.ts
+++ src/shared/models/routes.ts
@@ -4,10 +4,10 @@ import { TASK_ID_PARAM } from '../constants/constant-params';
 export enum TaskUrlEnum {
   base = 'tasks',
   getTasks = '/',
-  getTaskById = `/${TASK_ID_PARAM}`,
+  getTaskById = `/:${TASK_ID_PARAM}`,
   createTask = '/',
-  deleteTask = `/${TASK_ID_PARAM}`,
-  updateTaskStatus = `/${TASK_ID_PARAM}/status`,
+  deleteTask = `/:${TASK_ID_PARAM}`,
+  updateTaskStatus = `/:${TASK_ID_PARAM}/status`,
 }
 
 export enum AuthUrlEnum {
