a8dc6c19fabb801be51239df36eed10fb03bbc70
Guillaume Falvet <guillaume.falvet@outlook.com>
2024-04-21 17:31:47 +0200

    ✨ feat : using a parent/children route pathing

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,6 +9,8 @@ import { AuthModule } from './modules/auth/auth.module';
 // - Config - //
 import { typeOrmAsyncConfig } from './config/typeorm.config';
 import { envFileValidationSchema } from './config/env.schema';
+import { RouterModule } from '@nestjs/core';
+import { AuthUrlEnum, TaskUrlEnum } from './shared/models/routes';
 
 @Module({
   imports: [
@@ -20,6 +22,21 @@ import { envFileValidationSchema } from './config/env.schema';
     TypeOrmModule.forRootAsync(typeOrmAsyncConfig),
     TasksModule,
     AuthModule,
+    RouterModule.register([
+      {
+        path: 'api',
+        children: [
+          {
+            path: AuthUrlEnum.base,
+            module: AuthModule,
+          },
+          {
+            path: TaskUrlEnum.base,
+            module: TasksModule,
+          },
+        ],
+      },
+    ]),
   ],
 })
 export class AppModule {}
