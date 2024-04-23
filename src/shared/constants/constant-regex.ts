410ddff91efe31a9cba1710222f0f71fd8ba1e63
Guillaume Falvet <guillaume.falvet@outlook.com>
2024-04-23 19:58:49 +0200

    ✨ feat : added password regex

--- src/shared/constants/constant-regex.ts
+++ src/shared/constants/constant-regex.ts
@@ -12,6 +12,23 @@
  * - `\.`: Matches another literal dot, separating the second part of the JWT token from the third part.
  * - `[A-Za-z0-9-_.+/=]+`: Matches the third part of the JWT token (the signature), which is base64url-encoded and may contain alphanumeric characters, hyphens, underscores, dots, slashes, or equal signs. This part of the regex ensures that the signature is properly formatted.
  * - `$`: Asserts the end of the string.
+ *
+ * @type {RegExp}
  */
+
 export const JWT_HEADER_BEARER_REGEX =
   /^Bearer\s+[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+\.[A-Za-z0-9-_.+/=]+$/;
+
+/**
+ * Regular expression to validate the format of a password string.
+ *
+ * This regular expression ensures that the password:
+ * - Contains at least one digit or special character.
+ * - Does not contain periods or newline characters.
+ * - Contains at least one uppercase letter.
+ * - Contains at least one lowercase letter.
+ *
+ * @type {RegExp}
+ */
+export const PASSWORD_REGEX =
+  /((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/;
