8a2e2edf45cde746e5ea739e364c0f8996352c12
Falvet Guillaume <guillaume.falvet@gmail.com>
2024-05-02 11:32:42 +0200

    ✨ feat : improved swagger auth header persistance

--- src/docs/openAPI.ts
+++ src/docs/openAPI.ts
@@ -2,9 +2,10 @@ import { INestApplication, Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { SwaggerTheme, SwaggerThemeNameEnum } from 'swagger-themes';
-
 // - Models - //
 import { EnvEnum } from 'src/shared/models/env';
+
+// - Constants - //
 import { API_PATH_DOCS } from 'src/shared/constants/constant-path';
 
 export class OpenAPIDocumentationBuilder {
@@ -47,7 +48,7 @@ export class OpenAPIDocumentationBuilder {
       explorer: true,
       customCss: theme.getBuffer(SwaggerThemeNameEnum.DARK),
       swaggerOptions: {
-        plugins: [useSwaggerUIAuthStoragePlugin()],
+        plugins: [this._useSwaggerUIAuthStoragePlugin()],
       },
     };
     SwaggerModule.setup(API_PATH_DOCS, this.app, document, options);
@@ -55,94 +56,100 @@ export class OpenAPIDocumentationBuilder {
       `${this._HTTP_PROTOCOL}://${this._APP_HOST}:${this._APP_PORT}${API_PATH_DOCS}`,
     );
   }
-}
+  private _useSwaggerUIAuthStoragePlugin() {
+    /* eslint-disable */
+    // prettier-ignore
+    const afterLoad = function(ui) {
+      // NOTE: Code inside this afterLoad function will run in the browser!
+      //
+      // **Therefore, you cannot use an closure variables in here!**
+      // Also you should follow ES5 coding style.
+      //
+      // See: https://github.com/scottie1984/swagger-ui-express/blob/master/index.js#L239
+      //
+      // Other Notes:
+      // See https://github.com/scottie1984/swagger-ui-express/issues/44
+      // See https://github.com/swagger-api/swagger-ui/blob/master/src/core/system.js#L344
+      // See https://github.com/swagger-api/swagger-ui/issues/2915#issuecomment-297405865
+      var AUTH_SCHEME = "JWT-auth";
+      var currentAuthToken = undefined;
 
-function useSwaggerUIAuthStoragePlugin() {
-  /* eslint-disable */
-  // prettier-ignore
-  const afterLoad = function(ui) {
-    // NOTE: Code inside this afterLoad function will run in the browser!
-    //
-    // **Therefore, you cannot use an closure variables in here!**
-    // Also you should follow ES5 coding style.
-    //
-    // See: https://github.com/scottie1984/swagger-ui-express/blob/master/index.js#L239
-    //
-    // Other Notes:
-    // See https://github.com/scottie1984/swagger-ui-express/issues/44
-    // See https://github.com/swagger-api/swagger-ui/blob/master/src/core/system.js#L344
-    // See https://github.com/swagger-api/swagger-ui/issues/2915#issuecomment-297405865
-
-    var AUTH_SCHEME = "JWT-auth";
-    // const swaggerOptions = this;
-    var currentAuthToken = undefined;
-
-    setTimeout(function() {
-      // Restore auth token from localStorage, if any.
-      var token = localStorage.getItem(AUTH_SCHEME);
-      if (token) {
-        // check if the token is still valid
-        setAuthToken(token);
-        console.log("Restored " + AUTH_SCHEME + " token from localStorage.");
-      }
-      // Start polling ui.getState() to see if the user changed tokens.
-      setTimeout(checkForNewLogin, 3000);
-    }, 1000);
-
-    function checkForNewLogin() {
-      var stateToken = getAuthTokenFromState();
-      if (stateToken !== currentAuthToken) {
-        console.log("Saved " + AUTH_SCHEME + " token to localStorage.");
-        if (stateToken) {
-          localStorage.setItem(AUTH_SCHEME, stateToken);
-        } else {
-          localStorage.removeItem(AUTH_SCHEME);
+      setTimeout(function() {
+        // Restore auth token from localStorage, if any.
+        var token = localStorage.getItem(AUTH_SCHEME);
+        if (token) {
+          function isTokenExpired(token: string): boolean {
+            const expiry = (JSON.parse(atob(token.split('.')[1]))).exp;
+            return (Math.floor((new Date()).getTime() / 1000)) >= expiry;
+          }
+          if (isTokenExpired(token)) {
+            localStorage.removeItem(AUTH_SCHEME);
+            console.log("Token expired. Removed from localStorage.");
+            return;
+          }
+          setAuthToken(token);
+          console.log("Restored " + AUTH_SCHEME + " token from localStorage.");
         }
-        currentAuthToken = stateToken;
+        // Start polling ui.getState() to see if the user changed tokens.
+        setTimeout(checkForNewLogin, 3000);
+      }, 1000);
+
+      function checkForNewLogin() {
+        var stateToken = getAuthTokenFromState();
+        if (stateToken !== currentAuthToken) {
+          console.log("Saved " + AUTH_SCHEME + " token to localStorage.");
+          if (stateToken) {
+            localStorage.setItem(AUTH_SCHEME, stateToken);
+          } else {
+            localStorage.removeItem(AUTH_SCHEME);
+          }
+          currentAuthToken = stateToken;
+        }
+        // Continue checking every second...
+        setTimeout(checkForNewLogin, 1000);
       }
-      // Continue checking every second...
-      setTimeout(checkForNewLogin, 1000);
-    }
 
-    function getAuthTokenFromState() {
-      var state = ui.getState();
-      // Get token from state "auth.authorized[AUTH_SCHEME].value"
-      return getUIStateEntry(
-        getUIStateEntry(
-          getUIStateEntry(getUIStateEntry(state, "auth"), "authorized"),
-          AUTH_SCHEME
-        ),
-        "value"
-      );
-    }
-
-    function getUIStateEntry(state, name) {
-      if (state && state._root && Array.isArray(state._root.entries)) {
-        var entry = state._root.entries.find(e => e && e[0] === name);
-        return entry ? entry[1] : undefined;
+      function getAuthTokenFromState() {
+        var state = ui.getState();
+        ui
+        // Get token from state "auth.authorized[AUTH_SCHEME].value"
+        return getUIStateEntry(
+          getUIStateEntry(
+            getUIStateEntry(getUIStateEntry(state, "auth"), "authorized"),
+            AUTH_SCHEME
+          ),
+          "value"
+        );
       }
-      return undefined;
-    }
 
-    function setAuthToken(token) {
-      var authorization = {};
-      authorization[AUTH_SCHEME] = {
-        name: AUTH_SCHEME,
-        schema: {
-          type: "apiKey",
-          in: "header",
-          name: "Authorization",
-          description: "",
-        },
-        value: token,
-      };
-      var result = ui.authActions.authorize(authorization);
-      currentAuthToken = token;
-      return result;
-    }
-  };
-  /* eslint-enable */
-  return {
-    afterLoad,
-  };
+      function getUIStateEntry(state, name) {
+        if (state && state._root && Array.isArray(state._root.entries)) {
+          var entry = state._root.entries.find(e => e && e[0] === name);
+          return entry ? entry[1] : undefined;
+        }
+        return undefined;
+      }
+
+      function setAuthToken(token) {
+        var authorization = {};
+        authorization[AUTH_SCHEME] = {
+          name: AUTH_SCHEME,
+          schema: {
+            type: "apiKey",
+            in: "header",
+            name: "Authorization",
+            description: "",
+          },
+          value: token,
+        };
+        var result = ui.authActions.authorize(authorization);
+        currentAuthToken = token;
+        return result;
+      }
+    };
+    /* eslint-enable */
+    return {
+      afterLoad,
+    };
+  }
 }
